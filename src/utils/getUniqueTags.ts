1d79941c950f08e93f41aad01d731c7a6d194443
Sabrina <shemming444@gmail.com>
2025-02-01 18:00:07 -0800

    fix tag pagination / generation

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -7,17 +7,27 @@ interface Tag {
   tagName: string;
 }
 
-const getUniqueTags = (posts: CollectionEntry<'posts' | 'portfolio'>[]) => {
+const getUniqueTags = (
+  posts: CollectionEntry<'posts'>[],
+  portfolio: CollectionEntry<'portfolio'>[]
+) => {
   const tags: Tag[] = posts
     .filter(postFilter)
     .flatMap(post => post.data.tags)
-    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }))
+    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }));
+
+  let optTags: Tag[] = portfolio
+    .filter(postFilter)
+    .flatMap(post => post.data.tags)
+    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }));
+
+  return tags
+    .concat(optTags)
     .filter(
       (value, index, self) =>
         self.findIndex(tag => tag.tag === value.tag) === index
     )
     .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
-  return tags;
 };
 
 export default getUniqueTags;
